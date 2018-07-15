cff17833c8188b69c2101c2a2d9b24f3bc74e472
Kingder <zj809667533@live.com>
2018-07-16 01:39:23 +0800

    webpack内容

---
+++ noteBook/Vue.js/webPack的使用/project/src/main.js
@@ -0,0 +1,8 @@
+import $ from 'jquery'
+
+ $(function(){
+     $('li:odd').css('backgroundColor','lightblue');
+     $('li:even').css('backgroundColor',function(){
+         return "#"+"D97634"
+     })
+ })

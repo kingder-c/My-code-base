954107b693bb8899cc83b301df705a73f9e34c84
Kingder <zj809667533@live.com>
2018-08-19 22:19:26 +0800

    Add note

--- noteBook/node.js/httpService/03.readHtml.js
+++ noteBook/node.js/httpService/03.readHtml.js
@@ -5,37 +5,44 @@ const fs =require("fs");
 const path =require('path');
 
 http.createServer(function(req,res){
-    // res.setHeader
-    // if(req.url === '/login'){
-    //      fs.readFile(path.join(__dirname,'page','login.html'),function(err,data){
-    //          if(err){
-    //              throw err;
-    //          }
-    //          res.end(data);
-    //      })
-    // }
-    // else if(req.url === '/register'){
-    //     fs.readFile(path.join(__dirname,'page','register.html'),function(err,data){
-    //         if(err){
-    //             throw err;
-    //         }
-    //         res.end(data);
-    //     })
-    // }
-    // else{
-    //     fs.readFile(path.join(__dirname,'page','404.html'),function(err,data){
-    //         if(err){
-    //             throw err;
-    //         }
-    //         res.end(data);
-    //     })
-    // }
-    fs.readFile(path.join(__dirname,'page',req.url+'.html'),function(err,data){
-        if(err){
-            throw err;
-        }
-        res.end(data);
-    })
+    res.setHeader
+    if(req.url === '/login'){
+         fs.readFile(path.join(__dirname,'page','login.html'),function(err,data){
+             if(err){
+                 throw err;
+             }
+             res.end(data);
+         })
+    }
+    else if(req.url === '/register'){
+        fs.readFile(path.join(__dirname,'page','register.html'),function(err,data){
+            if(err){
+                throw err;
+            }
+            res.end(data);
+        })
+    }
+    else if(req.url === '/image/timg'){
+        fs.readFile(path.join(__dirname,'page','timg.jpg'),function(err,data){
+            res.setHeader("Content-Type","image/jpeg")
+            res.end(data)
+        });
+    }
+    else{
+        fs.readFile(path.join(__dirname,'page','404.html'),function(err,data){
+            if(err){
+                throw err;
+            }
+
+            res.end(data);
+        })
+    }
+    // fs.readFile(path.join(__dirname,'page',req.url+'.html'),function(err,data){
+    //     if(err){
+    //         throw err;
+    //     }
+    //     res.end(data);
+    // })
     console.log(req.url);
 }).listen(8080, function(){
     console.log("服务开启")
--- noteBook/node.js/httpService/04.tryCatch.js
+++ noteBook/node.js/httpService/04.tryCatch.js
@@ -1,12 +1,23 @@
 const fs=require('fs');
 
-fs.writeFile('./xxx/yyy.txt','大家早上好','utf-8',function(err){
-    if(err){
-        console.log(err)
-        throw err;
-    }
-    console.log('ok')
-})
+// fs.writeFile('./xxx/yyy.txt','大家早上好','utf-8',function(err){
+//     if(err){
+//         console.log(err)
+//         throw err;
+//     }
+//     console.log('ok')
+// })
 
+
+try {
+    fs.writeFile("./xxx/yyy.txt","大家早上好",'utf-8',function(error){
+        console.log("start");
+        console.log(error)
+    })
+    console.log("end")
+}
+catch(e){
+    console.log(e);
+}
 
 
